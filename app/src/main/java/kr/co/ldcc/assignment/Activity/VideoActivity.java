2298d0028f4be1316e18415103c8157510c0c46d
gyeom <koreatech93@naver.com>
2020-01-27 02:42:07 +0900

    ADD Dialog for writing comments

--- app/src/main/java/kr/co/ldcc/assignment/Activity/VideoActivity.java
+++ app/src/main/java/kr/co/ldcc/assignment/Activity/VideoActivity.java
@@ -1,8 +1,12 @@
 package kr.co.ldcc.assignment.Activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -46,6 +50,31 @@ public class VideoActivity extends YouTubeBaseActivity {
             }
 
         };
-                youtubeView.initialize(id, listener);
+
+        youtubeView.initialize(id, listener);
+
     }
+
+    public void writeBtnListener(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("댓글쓰기");
+        builder.setIcon(R.drawable.write);
+        // 다이얼로그를 통해 보여줄 뷰를 생성한다.
+        LayoutInflater inflater = getLayoutInflater();
+        View v1 = inflater.inflate(R.layout.dialog, null);
+        builder.setView(v1);
+
+        builder.setPositiveButton("작성하기",null);
+        builder.setNegativeButton("취소하기",null);
+
+        builder.show();
+    }
+
+    class DialogListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+
+        }
+    }
+
 }
